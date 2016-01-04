3ea337a5367c6371d96ed1e8bce4e5c1a8c7bd82
Carlos Hernández Gómez <karliky@gmail.com>
2016-01-04 20:42:39 +0100

    Add chai assertion lib

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 var craziness = require('../index');
-var assert = require('assert');
+var assert = require('chai').assert
+
 describe('Native', function() {
   it('should export native module correctly', function () {
     assert(craziness);
     assert(craziness.searchProcess);
+    assert.typeOf(craziness.searchProcess, 'function');
     assert(craziness.searchProcess('node.exe'));
   });
 });
