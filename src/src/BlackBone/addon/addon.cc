bafbff947e7982a4e743a17bab98a521ecf2907e
Carlos Hernández Gómez <karliky@gmail.com>
2016-01-04 22:57:19 +0100

    Add debug mode and tests

--- src/src/BlackBone/addon/addon.cc
+++ src/src/BlackBone/addon/addon.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <nan.h>
 #include <string>
+#include <sstream>
 
 #include "../Config.h"
 #include "../Process/Process.h"
@@ -11,22 +12,44 @@ using namespace blackbone;
 /// functions so we can reuse it
 Process proc;
 
+bool debugMode = false;
+
+void setDebugMode(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  bool mode = info[0]->BooleanValue();
+  debugMode = mode;
+  info.GetReturnValue().Set(debugMode);
+}
+
+void __log(std::string text) {
+  std::cout << "## " << text << std::endl;
+}
+
 void searchProcess(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 
   LPCWSTR processName = (LPCWSTR) * v8::String::Value(info[0]->ToString());
 
-  std::wcout << L"\r\nSearching for process...\n";
+  if (debugMode) __log("Searching for process...");
 
   std::vector<DWORD> found;
   Process::EnumByName( processName, found );
 
   if (found.size() > 0)
   {
-      std::wcout << L"\nProcess Found. Attaching to process " << std::dec << found.front() << std::endl;
+      if (debugMode) {
+        __log("Process Found. Attaching to process:");
+        std::stringstream sText;
+        sText << found.front();
+        __log(sText.str());
+      }
 
       if (proc.Attach( found.front() ) != STATUS_SUCCESS)
       {
-          std::wcout << L"\nCan't attach to process, status code " << LastNtStatus() << " aborting\n\n";
+        if (debugMode) {
+          __log("Can't attach to process, status code:");
+          std::stringstream sText;
+          sText << LastNtStatus();
+          __log(sText.str());
+        }
           return;
       }
 
@@ -40,6 +63,8 @@ void searchProcess(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 void Init(v8::Local<v8::Object> exports) {
   exports->Set(Nan::New("searchProcess").ToLocalChecked(),
 	  Nan::New<v8::FunctionTemplate>(searchProcess)->GetFunction());
+  exports->Set(Nan::New("setDebugMode").ToLocalChecked(),
+  	Nan::New<v8::FunctionTemplate>(setDebugMode)->GetFunction());
 }
 
 NODE_MODULE(BlackBone, Init)
--- test/test.js
+++ test/test.js
@@ -8,4 +8,23 @@ describe('Native', function() {
     assert.typeOf(craziness.searchProcess, 'function');
     assert(craziness.searchProcess('node.exe'));
   });
+
+  it('should activate debug mode', function () {
+    assert(craziness.setDebugMode);
+    assert.typeOf(craziness.setDebugMode, 'function');
+    assert(craziness.setDebugMode(true));
+  });
+
+  it('should deactivate debug mode', function () {
+    assert(craziness.setDebugMode);
+    assert.typeOf(craziness.setDebugMode, 'function');
+    assert.notOk(false, craziness.setDebugMode(false));
+  });
+
+  it('should not be true if no boolean is provided', function () {
+    assert(craziness.setDebugMode);
+    assert.typeOf(craziness.setDebugMode, 'function');
+    assert.notOk(false, craziness.setDebugMode('wololo'));
+  });
+
 });
